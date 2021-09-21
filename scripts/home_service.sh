f5bf5611abf39a2a1e2d2a99580bdf1fee9d6e47
aijim <suihe160@gmail.com>
2021-09-21 06:11:47 +0000

    change absolute paths to relative paths; removed source from scripts

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-xterm -e " export ROBOT_INITIAL_POSE='-x 0 -y -3 -Y 0'; source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/home_service_robot/worlds/my_room.world"&
+pwd
+
+xterm -e " export ROBOT_INITIAL_POSE='-x 0 -y -3 -Y 0';  roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/../worlds/my_room.world"&
 sleep 5
-xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/home_service_robot/map/map.yaml; source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch "&
+xterm -e "  export TURTLEBOT_GAZEBO_MAP_FILE=$(rospack find pick_objects)/../map/map.yaml; roslaunch turtlebot_gazebo amcl_demo.launch "&
 sleep 5
-xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch "&
+xterm -e "  roslaunch turtlebot_rviz_launchers view_navigation.launch "&
 sleep 5
-xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; rosrun add_markers add_markers"&
+xterm -e "  rosrun add_markers add_markers"&
 sleep 5
-xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch pick_objects pick_objects.launch"&
+xterm -e "  roslaunch pick_objects pick_objects.launch"&
